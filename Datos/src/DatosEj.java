ae562ece486092b6a317d28bfb6cac173bdb37e0
Andrés Otero <carlosandi18@gmail.com>
2025-02-14 08:49:58 -0500

    POO 1

---
+++ Datos/src/DatosEj.java
@@ -0,0 +1,32 @@
+
+public class DatosEj {
+    
+   public static void main(String[] args) {
+        /* System.out.println(" Andres Otero\n Andres \n Jhon Jairo \n Andres23");
+        */
+        
+      //Caracter  
+        
+/*  char Ejemplo = 'N';
+        
+            System.out.println("El caracter es: " + Ejemplo);
+        
+*/
+            
+        //Entero
+        
+        byte edad = 17;
+        System.out.println(edad);
+        
+        short anio = 2007;
+        System.out.println(anio);
+        
+        int numero = 34621;
+        System.out.println(numero);
+        
+        long celular = 310776189;
+        System.out.println(celular);
+        
+        
+    }
+}
